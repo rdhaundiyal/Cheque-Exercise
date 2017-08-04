b7e9dc451821f8fdb3dc12c7aa08480dc2a46f62
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2017-08-04 07:19:39 +0530

    Initial version

---
+++ Core/Logging/src/Deepend.Core.Logging/Log4NetAdaptor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Deepend.Core.Logging
+{
+    public class Log4NetLogger : ILogger
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+     (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public void Log(Exception ex)
+        {
+            log.Error(ex);
+        }
+
+        public void Log(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
