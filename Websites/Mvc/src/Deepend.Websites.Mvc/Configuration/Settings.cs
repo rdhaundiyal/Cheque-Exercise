fd67d5d0ffcf1909bdb226ea972cf0febc51afa7
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2017-08-04 11:43:15 +0530

    Added class for reading service base url from app setting

---
+++ Websites/Mvc/src/Deepend.Websites.Mvc/Configuration/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Deepend.Websites.Mvc.Configuration
+{
+    public class Settings
+    {
+        public static string ServiceBaseUrl { get { return ConfigurationManager.AppSettings["ServiceBaseUrl"]; } }
+    }
+}
