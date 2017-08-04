a818c5a7c7dd37e2cb1aa2eadf25b782990ded21
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2017-08-04 11:48:53 +0530

    Added Serialization unit test project

---
+++ Core/Serialization/test/Deepend.Core.Serialization.Test/TestSerialization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Deepend.Core.Serialization.Test
+{
+    [TestClass]
+    public class TestSerialization
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
